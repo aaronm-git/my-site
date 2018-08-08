5036356357dae6eb59e4a8c55510792026f87342
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2018-08-08 11:01:53 -0400

    responsive dates

--- content.php
+++ content.php
@@ -1,6 +1,7 @@
 <section class="post">
     <header class="post-header">
-        <h2 class="post-title"><?php the_title();?><small><?php echo get_the_date(); ?></small></h2>
+        <h2 class="post-title"><?php the_title();?></h2>
+        <p class="sub-text"><?php echo get_the_date(); ?></p>
         <p class="post-meta">By
             <a class="post-author" href="<?php echo get_the_author_meta($user_url); ?>"><?php the_author();?></a> under
             <?php foreach ((get_the_category()) as $category) {echo '<a class="post-category post-category-' . $category->slug . '" href="' . get_category_link($category->cat_ID) . '">' . $category->name . '</a>';}?>
