188dd7b39430c8d68a01283fda8a5c00507212a3
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2018-08-09 10:42:53 -0400

    added about info

--- page-about.php
+++ page-about.php
@@ -1,12 +1,149 @@
 <?php get_header();?>
-        <div class="posts">
-            <h1 class="content-subhead"><?php echo get_the_title(); ?></h1>
-        </div>
-        <h2>AARON MOLINA<small>frontend developer</small></h2>
-        <img alt="Aaron Molina&#x27;s avatar" class="main-avatar about" src="https://media.licdn.com/dms/image/C4E03AQEJjWZFFtJHRA/profile-displayphoto-shrink_200_200/0?e=1538611200&v=beta&t=oNV_woGG8AY6YMxoNDkWV-jFiKbtWqK3wGBPUgM881w">
-        <p>
-            Junior frontend developer looking for a place in the online tech industry. I recently graduated Ironhack, where I learned full-stack web development
-            (MongoDB, Express.js, Angular2, Node.js) and perfected my skills in HTML5, CSS3, and Javascript. I am very comfortable translating PSD files to HTML5,
-            sticking to the design as closely as possible while keeping in mind responsive design, SEO, and the overall user experience.
-        </p>
+        <h1 class="content-subhead"><?php echo get_the_title(); ?></h1>
+            <div>
+                <h2>AARON MOLINA</h2><h3 class="sub-text">Frontend Web Developer</h3>
+                        <p class="post-description">
+                            Junior frontend developer looking for a place in the online tech industry. I recently graduated Ironhack, where I learned full-stack web development
+                            (MongoDB, Express.js, Angular2, Node.js) and perfected my skills in HTML5, CSS3, and Javascript. I am very comfortable translating PSD files to HTML5,
+                            sticking to the design as closely as possible while keeping in mind responsive design, SEO, and the overall user experience.
+                        </p>
+                        <!-- <img alt="Aaron Molina&#x27;s avatar" class="main-avatar about" src="https://media.licdn.com/dms/image/C4E03AQEJjWZFFtJHRA/profile-displayphoto-shrink_200_200/0?e=1538611200&v=beta&t=oNV_woGG8AY6YMxoNDkWV-jFiKbtWqK3wGBPUgM881w"> -->
+            </div>
+            <div>
+                <h1 class="content-subhead">Professional Experience</h1>
+                <section>
+                    <h2>Freelance Frontend Developer & Web Consultant</h2>
+                    <h3 class="sub-text">Aug 2017 - PRESENT</h3>
+                    <p>Self-Employed</p>
+                    <p class="post-description">
+                        • Creating responsive and beautiful websites for individuals and businesses alike.<br>
+                        • Able to build WordPress themes from scratch.<br>
+                        • Hosting websites for clients using secure and reliable servers in the US.<br>
+                        • Helping clients get the best out of their business by creating them a reliable online presence.<br>
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Senior Support Tech (Help Desk)</h2>
+                    <h3 class="sub-text">May 2016 - Aug 2017</h3>
+                    <p>PracticePanther</p>
+                    <p class="post-description">
+                        Troubleshooting issues and provide guidance, training, and working solutions to problems.<br>
+                        <br>
+                        Accomplishments:<br>
+                        • Resolving over 50 cases per day while upholding a high above 90% CSAT and positive NPS scores.<br>
+                        • Quickly rose to highest tier support in a few months because of my quick understanding of issues, swiftly resolving them, and for providing the best workarounds.<br>
+                        • Find, replicate, and document bugs for the development team to fix.<br>
+                        • Specially worked with the development team in a Beta environment to help the team troubleshoot issues, find bugs, and break the software before publishing new features to production.<br>
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Online Marketing Specialist</h2>
+                    <h3 class="sub-text">Aug 2015 - May 2016</h3>
+                    <p>Florida Swimming Pools, LLC</p>
+                    <p class="post-description">
+                        • Launched business online on Google Maps, Google My Business, Yahoo, Bing Places, etc.<br>
+                        • Created and managed AdWords campaigns to generate over 35 leads a week.<br>
+                        • Managed SEO strategy.<br>
+                        • Email Blast leads using MailChimp.<br>
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Consultation Agent</h2>
+                    <h3 class="sub-text">Jun 2014 - Aug 2015</h3>
+                    <p>GeekSquad</p>
+                    <p class="post-description">
+                        The solution expert, managing the check-in and check-out experience for clients and helping find complete solutions for their tech problems. I also coached the sales team, building trust and explaining services and technology to help staff match available services to the challenges customers were facing.<br>
+                        <br>
+                        Accomplishments:<br>
+                        • Out of the 5 stores in the district, I was #1 in sales revenue for GeekSquad in the quarter.<br>
+                        • Assigned to provide Windows 10 software classes to teach consumer about the advantages of using Windows 10 and encourage upgrading.<br>
+                        • Consistently generated GeekSquad over $10,000-$12,000 in revenue each month.<br>
+                        • Continually recognized by management for achieving set NPS scores, revenue goals, and setting customer service standards with the team. See less<br>
+                    </p>
+                </section>
+            </div>
+
+            <div>
+                <h1 class="content-subhead">Skills</h1>
+                <div class="pure-g">
+                    <section class="pure-u-1 pure-u-sm-1-2">
+                        <h2>Advanced: <small>(4 years)</small></h2>
+                        <ul class="post-description">
+                            <li>HTML5 & CSS3</li>
+                            <li>JavaScript, jQuery</li>
+                            <li>
+                                CSS Frameworks:
+                                <ul>
+                                    <li><a href="https://getbootstrap.com/" target=_blank>Boostrap</a></li>
+                                    <li><a href="https://foundation.zurb.com/" target=_blank>Foundation</a></li>
+                                    <li><a href="https://purecss.io/" target=_blank>PureCSS</a></li>
+                                    <li><a href="https://bulma.io/" target=_blank>Bulma</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="pure-u-1 pure-u-sm-1-2">
+                        <h2>Junior: <small>(1 year)</small></h2>
+                        <ul class="post-description">
+                            <li>PHP</li>
+                            <li>MySQL</li>
+                            <li>Angular2</li>
+                            <li>Node.js</li>
+                            <li>Express.js</li>
+                            <li>MongoDB</li>
+                        </ul>
+                    </section>
+                </div>
+
+            </div>
+
+            <div>
+                <h1 class="content-subhead">Education</h1>
+                <section>
+                    <h2>Ironhack</h2>
+                    <h3 class="sub-text">2017 - 2018</h3>
+                    <p class="post-description">
+                        <b>Learned how to create beautiful user experiences using HTML, CSS, and JavaScript. Topics include:</b><br>
+                        <br>
+                        • Dev. Environment Setup (Local)<br>
+                        • Introduction to algorithms with JavaScript<br>
+                        • Interface creation with HTML5 and CSS3<br>
+                        • JavaScript libraries & jQuery<br>
+                        • Project collaboration using GitHub<br>
+                        <br>
+                        <b>Learned MongoDB, Express.js, Angular, and Node.js (MEAN stack) to create non-relational database-driven applications. Topics include:</b><br>
+                        <br>
+                        • Node.js and asynchronous programming in JavaScript<br>
+                        • Web applications using the Express.js framework<br>
+                        • Non-relational databases (MongoDB) & ORMs<br>
+                        • Application deployment following best practices<br>
+                        <br>
+                        <b>Learned how to create APIs (Application Programming Interfaces) to design microservice architecture. I also learned how to build applications using Angular2. Topics include:</b><br>
+                        <br>
+                        • API Design with Node.js<br>
+                        • MVC Application design with Angular2<br>
+                        • Creating web components<br>
+                        • External APIs with the MEAN Stack<br>
+                    </p>
+                </section>
+
+                <section>
+                    <h2>University of Miami</h2>
+                    <h3 class="sub-text">2011 - 2012</h3>
+                    <p class="post-description">
+                        <b>Web Developent and Design</b>
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Miami Dade Collage</h2>
+                    <h3 class="sub-text">2009 - 2011</h3>
+                    <p class="post-description">
+                        <b>Computer Science</b>
+                    </p>
+                </section>
+            </div>
 <?php get_footer();?>
